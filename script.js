757ed1917e430c7fa05a227ffdc8521f657998fb
Tibedo <thibaudbaily@hotmail.com>
2022-05-05 14:37:57 +0200

    interval to do

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let booster = document.getElementById("booster")
-booster.addEventListener("click", openBooster)
 
 
 function randomNum(min, max) {
@@ -10,28 +8,22 @@ function randomNum(min, max) {
 //    location.reload()
 //}
 
+
+/*
 function openBooster() {
 
     let openedCard = document.getElementById("booster-open");
     while (openedCard.firstChild) {
         openedCard.firstChild.remove();
     }
-
-    let arr = []
-
-    for(let i = 0; i < 11; i++)  {
-        
-        //setInterval(function() {
-        
+     
         let cardDiv = document.createElement("div")
         cardDiv.className = "pokemon-card"
-       // cardDiv.style.visibility = "hidden"
+       
 
         let pokemon = document.createElement("img")
         pokemon.id = i
-       // pokemon.style.visibility = "hidden"
 
-        arr.push(cardDiv)
 
         let num = 1;
          if (i == 10) {
@@ -46,23 +38,58 @@ function openBooster() {
         
         document.getElementById("booster-open").appendChild(cardDiv)
 
-        //}, 1000)
-       console.log(arr)
+    }
+    setInterval(openBooster(), 1000);
+*/
+
+
+
+let i = 0
+const MAX = 10
+
+let go = function() {
+
+        function create() {
+        let cardDiv = document.createElement("div")
+        cardDiv.className = "pokemon-card"
+       
+
+        let pokemon = document.createElement("img")
+        let x = 1;
+        pokemon.id = i;
+        i++
+   
+        let num = 1;
+         if (i == 11) {
+             num = randomNum(1, 16)
+         }
+         else {
+             num = randomNum(17, 102)
+         }
+        pokemon.src = "./pokemon-cards/base set (" + num.toString() + ").jpg"
+        
+        cardDiv.appendChild(pokemon)
+
+        
+        document.getElementById("booster-open").appendChild(cardDiv)
+    }
+    create()
     }
 
-   // setInterval(function() {
-   //     cardDiv = document.getElementsByClassName("pokemon-card")
-   //     cardDiv[0].style.visibility = "display"
-       
-  //  }, 1000)
-    
-    
-}
+        function inter() {
+            let timeout 
 
+            if (i === 10) {
+             timeout = setInterval(go(), 6000)    
+             } 
+            else if ( i < MAX)   {
+                timeout = setInterval(go(), 1000);
+            }     
+            else {
+            clearInterval(timeout)
+            }
+            
+        }
 
-
-//let buy = document.createElement("button")
-//    buy.id = "buy"
-//    buy.textContent = "Buy New Pack"
-//   buy.addEventListener("click", buyBooster)
-//   document.body.appendChild(buy)
+    let booster = document.getElementById("booster")
+    booster.addEventListener("click", inter)
